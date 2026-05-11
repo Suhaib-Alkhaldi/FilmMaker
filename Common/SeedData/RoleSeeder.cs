63db290eaf9c1edca60fb7efefa6dedfeeb12e68
Shatha Ammar <shathaammar407@gmail.com>
2026-05-11 12:27:45 +0300

    Add RoleSeeder and move role seeding from LookupSeeder.

---
+++ Common/SeedData/RoleSeeder.cs
@@ -0,0 +1,25 @@
+﻿using FilmMaker.Entities;
+
+namespace FilmMaker.Common.SeedData
+{
+    public static class RoleSeeder
+    {
+        public static void Seed(FilmMakerDbContext context)
+        {
+            if (context.Roles.Any())
+                return;
+
+            var roles = new List<Role>
+        {
+            new() { Name = "Admin" },
+            new() { Name = "Location Owner" },
+            new() { Name = "Location Manager" },
+            new() { Name = "Production Company" },
+            new() { Name = "Service Provider" }
+        };
+
+            context.Roles.AddRange(roles);
+            context.SaveChanges();
+        }
+    }
+}
