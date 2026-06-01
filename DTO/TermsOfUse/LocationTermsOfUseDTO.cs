bec2f9dc83f50a705fa4e909ee1f683f4cda79ac
omarhattab1 <omar.hattab@unlimited-innovation.com>
2026-06-01 14:28:56 +0300

    removed from branch DTO/TermsOfUse/LocationTermsOfUseDTO.cs

--- DTO/TermsOfUse/LocationTermsOfUseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FilmMaker.DTO.TermsOfUse
-{
-    public class LocationTermsOfUseDTO
-    {
-        public int Id { get; set; }
-        public int LocationId { get; set; }
-        public string Term { get; set; } = string.Empty;
-    }
-}
